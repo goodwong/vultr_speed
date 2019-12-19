e1638190a07f643ddc1267077c60828ec340ccb6
William Wong <goodwong@foxmail.com>
2019-12-19 08:22:29 +0000

    脚本方式

---
+++ scripts/my_speed_download.sh
@@ -0,0 +1,51 @@
+printf "\n\n开始时间：`date`"
+
+printf "\n\n\033[1;33m【Frankfurt, DE】\033[0m\n"
+curl -o /dev/null --max-time 50 https://fra-de-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Paris, France】\033[0m\n"
+curl -o /dev/null --max-time 50 https://par-fr-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Amsterdam, NL】\033[0m\n"
+curl -o /dev/null --max-time 50 https://ams-cl-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【London, UK】\033[0m\n"
+curl -o /dev/null --max-time 50 https://lon-gb-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【New York (NJ)】\033[0m\n"
+curl -o /dev/null --max-time 50 https://nj-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Singapore】\033[0m\n"
+curl -o /dev/null --max-time 50 https://sgp-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Toronto, Canada】\033[0m\n"
+curl -o /dev/null --max-time 50 https://tor-ca-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Chicago, Illinois】\033[0m\n"
+curl -o /dev/null --max-time 50 https://il-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Atlanta, Georgia】\033[0m\n"
+curl -o /dev/null --max-time 50 https://ga-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Miami, Florida】\033[0m\n"
+curl -o /dev/null --max-time 50 https://fl-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Tokyo, Japan】\033[0m\n"
+curl -o /dev/null --max-time 50 https://hnd-jp-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Dallas, Texas】\033[0m\n"
+curl -o /dev/null --max-time 50 https://tx-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Seattle, Washington】\033[0m\n"
+curl -o /dev/null --max-time 50 https://wa-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Silicon Valley, California】\033[0m\n"
+curl -o /dev/null --max-time 50 https://sjo-ca-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Los Angeles, California】\033[0m\n"
+curl -o /dev/null --max-time 50 https://lax-ca-us-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n\033[1;33m【Sydney, Australia】\033[0m\n"
+curl -o /dev/null --max-time 50 https://syd-au-ping.vultr.com/vultr.com.100MB.bin
+
+printf "\n\n完成时间：`date`\n\n"
---
+++ scripts/my_speed_ping.sh
@@ -0,0 +1,51 @@
+printf "\n\n开始时间：`date`"
+
+printf "\n\n\033[1;33m【Frankfurt, DE】\033[0m\n"
+ping -c 100 -q fra-de-ping.vultr.com
+
+printf "\n\n\033[1;33m【Paris, France】\033[0m\n"
+ping -c 100 -q par-fr-ping.vultr.com
+
+printf "\n\n\033[1;33m【Amsterdam, NL】\033[0m\n"
+ping -c 100 -q ams-cl-ping.vultr.com
+
+printf "\n\n\033[1;33m【London, UK】\033[0m\n"
+ping -c 100 -q lon-gb-ping.vultr.com
+
+printf "\n\n\033[1;33m【New York (NJ)】\033[0m\n"
+ping -c 100 -q nj-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Singapore】\033[0m\n"
+ping -c 100 -q sgp-ping.vultr.com
+
+printf "\n\n\033[1;33m【Toronto, Canada】\033[0m\n"
+ping -c 100 -q tor-ca-ping.vultr.com
+
+printf "\n\n\033[1;33m【Chicago, Illinois】\033[0m\n"
+ping -c 100 -q il-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Atlanta, Georgia】\033[0m\n"
+ping -c 100 -q ga-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Miami, Florida】\033[0m\n"
+ping -c 100 -q fl-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Tokyo, Japan】\033[0m\n"
+ping -c 100 -q hnd-jp-ping.vultr.com
+
+printf "\n\n\033[1;33m【Dallas, Texas】\033[0m\n"
+ping -c 100 -q tx-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Seattle, Washington】\033[0m\n"
+ping -c 100 -q wa-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Silicon Valley, California】\033[0m\n"
+ping -c 100 -q sjo-ca-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Los Angeles, California】\033[0m\n"
+ping -c 100 -q lax-ca-us-ping.vultr.com
+
+printf "\n\n\033[1;33m【Sydney, Australia】\033[0m\n"
+ping -c 100 -q syd-au-ping.vultr.com
+
+printf "\n\n完成时间：`date`\n\n"

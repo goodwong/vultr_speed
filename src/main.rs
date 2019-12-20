ba5ae7cd34ccc8c1d230fdac5ddc0c662c912785
William Wong <goodwong@foxmail.com>
2019-12-20 15:03:47 +0000

    改造成生成消费者模式

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,64 @@
+use async_std::sync::{Arc, Mutex};
 use bytesize::ByteSize;
 use reqwest;
-//use std::io::{stdout, Write};
-use std::time::Instant;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://wa-us-ping.vultr.com/vultr.com.100MB.bin";
-    reqwest_download(url).await
+    let stats = Arc::new(Mutex::from(vec![]));
+
+    // 启动下载
+    let receiver = stats.clone();
+    tokio::spawn(async move {
+        download(url, receiver).await.unwrap();
+    });
+
+    // 输出
+    let mut last_time = 0;
+    loop {
+        tokio::timer::delay_for(Duration::from_millis(1000)).await;
+
+        let mut logs: Vec<(u128, usize)> = stats.lock().await.drain(0..).collect();
+        // println!("logs: {:?}", logs.clone());
+        let bytes = logs.iter().cloned().fold(0, |a, b| a + b.1);
+
+        // 如果没有日志的话，end 就是当前时间戳
+        // 有日志的话，就以最后一个日志的时间为准
+        let end = match logs.pop() {
+            Some((t, _)) => t,
+            None => SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis(),
+        };
+        let duration = end - last_time;
+        // println!("{}, {}, {}, {}", last_time, end, duration, bytes);
+        last_time = end;
+        let speed = if duration > 0 {
+            bytes / (duration as usize) * 1000
+        } else {
+            0
+        };
+        print!("Speed: {:>9} ", ByteSize::b(speed as u64).to_string());
+        println!(" {:<30}", "█".repeat(30 * speed as usize / 2_000_000));
+    }
 }
 
-async fn reqwest_download(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn download(
+    url: &str,
+    receiver: Arc<Mutex<Vec<(u128, usize)>>>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut res = reqwest::get(url).await?;
 
-    let time_start = Instant::now();
-    let mut time_previous = 0;
-    let mut downloaded = 0;
-    let mut bytes_batch = 0;
-    let content_length = res.content_length();
-
-    // todo 其实应该独立一个线程每秒计算的，
-    // todo 否则网络卡死了，瞬时速度不会归零
-    // todo 暂时这样做
     while let Some(chunk) = res.chunk().await? {
-        let now = time_start.elapsed().as_secs();
-
-        bytes_batch += chunk.len() as u64;
-        if now > time_previous {
-            // 计算
-            downloaded += bytes_batch;
-            let speed_current = bytes_batch / (now - time_previous);
-            let speed_average = downloaded / now;
-            // 重置计数器
-            bytes_batch = 0;
-            time_previous = now;
-
-            // 输出
-            //print!("\r");
-            print!(
-                "Speed: {:>9}/{:<9}; ",
-                ByteSize::b(speed_current).to_string(),
-                ByteSize::b(speed_average).to_string()
-            );
-            print!("Progress: {:>9}", ByteSize::b(downloaded).to_string());
-            match content_length {
-                Some(lenth) => {
-                    print!("/{:<9}", ByteSize::b(lenth).to_string());
-                }
-                None => {
-                    print!("/-        ");
-                }
-            };
-            //stdout().flush().unwrap();
-
-            println!(
-                " {:<30}",
-                "█".repeat(30 * speed_current as usize / 2_000_000)
-            );
-        }
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+        let len = chunk.len();
+        receiver.lock().await.push((now, len));
     }
-    println!("\nDone!");
     Ok(())
 }

fbd27a81da7bf7760ab135e7baed58c0021b311f
William Wong <goodwong@foxmail.com>
2019-12-24 09:31:08 +0000

    按颜色区分速度

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use async_std::sync::{Arc, Mutex};
 use bytesize::ByteSize;
 use chrono;
 use reqwest;
-use std::env;
 use std::io::{stdout, Write};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use termion;
@@ -93,13 +92,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     ];
     let logs = Arc::new(Mutex::from(vec![]));
 
-    let column_width = 12;
-    let args: Vec<String> = env::args().collect();
-    let max_speed: usize = match args.get(1) {
-        Some(arg) => arg.parse()?,
-        None => 2_000_000,
-    };
-    println!("  --max_speed: {}", max_speed);
+    let column_width: usize = 12;
 
     let mut loop_index: i32 = 0;
     loop {
@@ -152,7 +145,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     print!("{}", termion::cursor::Down(1));
                     print!("{}", termion::cursor::Left(column_width as u16));
                 }
-                print_speed(speed, max_speed, column_width);
+                print_speed(speed, column_width);
                 stdout().flush()?;
             }
         }
@@ -191,23 +184,108 @@ fn now() -> u128 {
         .as_millis()
 }
 
-fn print_speed(speed: usize, max: usize, width: usize) {
+fn print_speed(speed: usize, column_width: usize) {
+    // 查看所有颜色命令：for x in {0..8}; do for i in {30..37}; do for a in {40..47}; do echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "; done; echo; done; done; echo ""
+    // 参见：https://askubuntu.com/questions/27314/script-to-display-all-terminal-colors
+    //
+    // 速率UI：
+    //    0     ~    100kB   0格  100/ 白色段（\e[0;30;47m）
+    //    100kB ~    200kB   1格  100/
+    //    200kB ~    400kB   2格  200/
+    //    400kB ~    600kB   3格  200/
+    //    600kB ~  1,000kB   4格  400/
+    //  1,000kB ~  1,400kB   5格  400/
+    //  1,400kB ~  2,200kB   6格  800/
+    //  2,200kB ~  3,000kB   7格  800/ 紫色段（\e[0;30;45m）
+    //  3,000kB ~  4,600kB   8格 1600/
+    //  4,600kB ~  6,200kB   9格 1600/
+    //  6,200kB ~  9,400kB  10格 3200/ 红色段（\e[0;30;41m）
+    //  9,400kB ~ 12,600kB  11格 3200/
+    // 12,600kB ~ 19,000kB  12格 6400/
+    //
+
     let output = format!(
         "{:<width$}",
         ByteSize::b(speed as u64).to_string() + "/s",
-        width = width
+        width = column_width
     );
-    let speed_bar_len = usize::min(width, width * speed as usize / max);
-    let (part1, part2) = if speed >= max {
-        (
-            format!("\x1B[7;31m{}\x1B[0m", &output[0..speed_bar_len]),
-            format!("\x1B[31;m{}\x1B[0m", &output[speed_bar_len..]),
-        )
-    } else {
-        (
-            format!("\x1B[7m{}\x1B[0m", &output[0..speed_bar_len]),
-            format!("\x1B[0m{}\x1B[0m", &output[speed_bar_len..]),
-        )
+
+    let white = "\x1B[0;30;47m";
+    let reset = "\x1B[0m";
+    let magenta = "\x1B[0;30;45m";
+    let red = "\x1B[0;30;41m";
+
+    let bar = match speed {
+        // 白色段
+        0..=100_000 => format!("{}", &output),
+        100_000..=200_000 => format!("{}{}{}{}", white, &output[0..1], reset, &output[1..]),
+        200_000..=400_000 => format!("{}{}{}{}", white, &output[0..2], reset, &output[2..]),
+        400_000..=600_000 => format!("{}{}{}{}", white, &output[0..3], reset, &output[3..]),
+        600_000..=1_000_000 => format!("{}{}{}{}", white, &output[0..4], reset, &output[4..]),
+        1_000_000..=1_400_000 => format!("{}{}{}{}", white, &output[0..5], reset, &output[5..]),
+        1_400_000..=2_200_000 => format!("{}{}{}{}", white, &output[0..6], reset, &output[6..]),
+        // 紫色段
+        2_200_000..=3_000_000 => format!(
+            "{}{}{}{}{}{}",
+            white,
+            &output[0..6],
+            magenta,
+            &output[6..7],
+            reset,
+            &output[7..]
+        ),
+        3_000_000..=4_600_000 => format!(
+            "{}{}{}{}{}{}",
+            white,
+            &output[0..6],
+            magenta,
+            &output[6..8],
+            reset,
+            &output[8..]
+        ),
+        4_600_000..=6_200_000 => format!(
+            "{}{}{}{}{}{}",
+            white,
+            &output[0..6],
+            magenta,
+            &output[6..9],
+            reset,
+            &output[9..]
+        ),
+        // 红色段
+        6_200_000..=9_400_000 => format!(
+            "{}{}{}{}{}{}{}{}",
+            white,
+            &output[0..6],
+            magenta,
+            &output[6..9],
+            red,
+            &output[9..10],
+            reset,
+            &output[10..]
+        ),
+        9_400_000..=12_600_000 => format!(
+            "{}{}{}{}{}{}{}{}",
+            white,
+            &output[0..6],
+            magenta,
+            &output[6..9],
+            red,
+            &output[9..11],
+            reset,
+            &output[11..]
+        ),
+        12_600_000..=19_000_000 => format!(
+            "{}{}{}{}{}{}{}",
+            white,
+            &output[0..6],
+            magenta,
+            &output[6..9],
+            red,
+            &output[9..12],
+            reset
+        ),
+        _ => format!("{}{}{}", red, &output, reset),
     };
-    print!("{}{}", part1, part2);
+    print!("{}", bar);
 }
